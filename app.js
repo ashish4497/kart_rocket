2bcaf8e38e9f86b043f7ffbdbdd20114d9c228ab
ashish4497 <ak8393267@gmail.com>
2019-07-19 16:13:27 +0530

    fix port number

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 var webpack = require("webpack");
-const port = 8000;
-// process.env.PORT ||
+const port = process.env.PORT || 8000;
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) => {
